41ef0627d9bfc4133c62a72a87e1ac152a75d648
Indla Keerthi Sree <indlakeerthisree25@gmail.com>
2026-03-25 09:21:50 +0530

    Time: 4 ms (53.14%), Space: 45.2 MB (83.1%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -2,34 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] finalPrices(int[] prices) {
-        
         int n = prices.length;
-        int[] answer = Arrays.copyOf(prices, n);
+        int[] result = Arrays.copyOf(prices, n);
         
-        Stack<Integer> stack = new Stack<>(); // store indexes
+        Stack<Integer> stack = new Stack<>(); // store indices
         
         for (int i = 0; i < n; i++) {
-            
             while (!stack.isEmpty() && prices[i] <= prices[stack.peek()]) {
-                
-                int prevIndex = stack.pop();
-                answer[prevIndex] = prices[prevIndex] - prices[i];
+                int index = stack.pop();
+                result[index] = prices[index] - prices[i];
             }
-            
             stack.push(i);
         }
         
-        return answer;
-    }
-
-    // For testing
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        int[] prices = {8,4,6,2,3};
-        
-        int[] result = sol.finalPrices(prices);
-        
-        System.out.println(Arrays.toString(result));
+        return result;
     }
 }
